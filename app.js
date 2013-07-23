7d25e00dbb819d5f0a56301afc8aa53b876c7e96
Ben Chavet <ben.chavet@lullabot.com>
2013-07-23 16:34:49 -0500

    first stab at LDAP auth. passport-ldap needs some love

---
+++ app.js
@@ -0,0 +1,62 @@
+// Module Dependencies
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    mysql = require('mysql'),
+    fs = require('fs'),
+    glob = require('glob'),
+    socket = require('socket.io'),
+    passport = require('passport');
+
+// Local Dependencies
+var routes = require('./routes'),
+    config = require('./config.json');
+
+// Application Initializations
+var app = express(),
+    server = http.createServer(app),
+    io = socket.listen(server, { 'log level': 2 }),
+    db = mysql.createConnection(config.mysql);
+
+// Session Handlers
+var cookie = express.cookieParser(config.session.secret),
+    store = new express.session.MemoryStore(),
+    session = express.session({ secret: config.session.secret, key: config.session.key, store: store });
+
+// Passport Configuration
+var LDAPStrategy = require('./lib/passport-ldap').Strategy
+passport.use(new LDAPStrategy(config.ldap, function(profile, done) {
+    return done(null, JSON.parse(profile));
+}));
+
+// Application Configuration
+app.configure(function() {
+    app.set('port', process.env.PORT || config.server.port);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'twig');
+
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.bodyParser());
+
+    // Enable express sessions
+    app.use(cookie);
+    app.use(session);
+
+    // Enable passport middleware
+    app.use(passport.initialize());
+//    app.use(passport.session());
+
+    // Configure twig.js
+    app.set('twig options', { strict_variables: false });
+});
+
+// Routes
+app.get('/', routes.index);
+app.get('/login', routes.login);
+app.post('/login', passport.authenticate('ldap', { successRedirect: '/',
+                                                   failureRedirect: '/login' }));
+
+// Start the express server
+server.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+exports.index = function(db, config) {
+    return function(req, res) {
+    }
+}
+
+exports.login = require('./login');
